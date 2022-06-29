d63ffe9a2b29febdc0b126882c06accffd873fff
Faisal Momin <mfmomin84@gmail.com>
2022-06-29 23:16:42 +0530

    ok

---
+++ DealDouble.Web/Controllers/AuctionsController.cs
@@ -0,0 +1,56 @@
+﻿using DealDouble.Entities;
+using DealDouble.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace DealDouble.Web.Controllers
+{
+    public class AuctionsController : Controller
+    {
+        // GET: Auctions
+        public ActionResult Index()
+        {
+            AuctionsService service = new AuctionsService();
+            var auctions = service.GetAllAuction();
+            return View(auctions);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Auction auction)
+        {
+            AuctionsService service = new AuctionsService();
+
+            service.SaveAuction(auction);
+
+            return View();
+        }
+
+        // GET: Auctions
+        public ActionResult Edit(int ID)
+        {
+            AuctionsService service = new AuctionsService();
+
+            var auction =  service.GetAuctionByID(ID);
+
+            return View(auction);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Auction auction)
+        {
+            AuctionsService service = new AuctionsService();
+
+            service.UpdateAuction(auction);
+
+            return View(auction);
+        }
+    }
+}
